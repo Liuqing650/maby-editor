060a43eb2838e4cdffa4e92a6cbcce36839876d4
Liuqing650 <1006822621@qq.com>
2018-10-22 18:08:34 +0800

    修复入口文件

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import NewCom from './components/new';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+import { BoldPlugin, BlockquotePlugin } from './plugins';
+// initSate
+import * as initState from './initValue/initState';
+// style
 import './style';
 
-class MabyLibCli extends React.Component {
-  state = {
-    value: 'maby-lib-cli'
-  };
+const SAVE_KEY = 'maybe-slate-save';
+// 获取本地缓存数据
+const existingValue = localStorage.getItem(SAVE_KEY) ? JSON.parse(localStorage.getItem(SAVE_KEY)) : null;
+// 构建初始状态…
+const initialState = existingValue ? Value.fromJSON(existingValue) : initState.valueModel('A line of text in a paragraph.');
+
+// 插件
+const plugins = [
+  BoldPlugin(),
+  BlockquotePlugin(),
+];
+
+class MaybeEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: initialState
+    };
+  }
+  componentDidMount = () => {
+    const { editor } = this;
+    if (editor) {
+      setTimeout(editor.focus.bind(editor), 1000);
+    }
+  }
+  // 发生变更时，使用新的编辑器状态更新应用的 React 状态。
+  onChange = ({ value }) => {
+    this.setState({ value });
+  }
+
   render() {
-    const { value } = this.state;
-    const { sendMsg } = this.props;
+    const { placeholder, className } = this.props;
     return (
-      <div>
-        <div>{value}</div>
-        <i>{sendMsg}</i>
-        <NewCom />
+      <div className={className}>
+        <Editor
+          plugins={plugins}
+          placeholder={placeholder || ''}
+          value={this.state.value}
+          onChange={this.onChange}
+          ref={(element) => { this.editor = element; }}
+        />
       </div>
     );
   }
 }
-export default MabyLibCli;
+export default MaybeEditor;
