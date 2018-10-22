89a3b74c1c25ad587b7d4ef64d7daee214e4cb13
Liuqing650 <1006822621@qq.com>
2018-10-22 18:11:41 +0800

    合并分支

--- example/example.js
+++ example/example.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import MabyLibCli from '../src';
+import MabyEditor from '../src';
 
 export default class Example extends Component {
+  state = {
+    value: null
+  }
+  onChange = ({ value }) => {
+    // console.log('value---->', value);
+    this.setState({ value });
+  }
+  handleSubmit = (value) => {
+    console.log(value);
+  }
   render() {
+    const { value } = this.state;
+    const debug = false;
+    if (debug) {
+      console.log('value----->', value);
+    }
+    const mabyEditorProps = {
+      placeholder: '输入一些文本吧',
+      className: 'myEditor'
+    };
     return (
       <div>
-        <h2>maby-lib-cli</h2>
-        <MabyLibCli sendMsg="hello lib!" />
+        <MabyEditor {...mabyEditorProps} />
       </div>
     );
   }
